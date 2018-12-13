a7aa1e92b1f76cd5ccb40361f0372ca342bce7f0
Artiom K <artiom.kornilov@gmail.com>
2018-12-13 13:29:56 +0200

    launched Hello World via springboot (WOW)

--- src/main/java/lect16/spring/AppConfig.java
+++ src/main/java/lect16/spring/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({ "lect.spring.*" })
+@ComponentScan({ "lect16.*" })
 public class AppConfig {
 
 
--- src/main/java/lect16/springboot/AppSpringBoot.java
+++ src/main/java/lect16/springboot/AppSpringBoot.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "lect16.spring.*" })
+@ComponentScan({ "lect16.*" })
 public class AppSpringBoot {
 
     public static void main(String[] args) {
